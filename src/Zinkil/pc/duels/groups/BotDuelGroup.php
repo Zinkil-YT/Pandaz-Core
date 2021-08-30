b1d0368dfd9c932abdb549a0d4d9b5039cb7ed74
ProtectorYT364 <86392268+ProtectorYT364@users.noreply.github.com>
2021-08-30 13:50:43 +0530

    Bots will have sword now :p

--- src/Zinkil/pc/duels/groups/BotDuelGroup.php
+++ src/Zinkil/pc/duels/groups/BotDuelGroup.php
@@ -306,8 +306,11 @@ class BotDuelGroup{
 				$player->setHealth(20);
 			}
 			if($this->isBotOnline()){
-				if($this->isBotOnline()) $this->getBot()->setDeactivated(true);
+				if($this->isBotOnline()){
+				if($this->isBotOnline()) $this->getBot()->setDeactivated(false);
 				if($this->isBotOnline()) Kits::sendMatchKit($this->getBot(), "NoDebuff");
+				if($this->isBotOnline()) $this->getBot()->getInventory()->setItemInHand(Item::get(Item::DIAMOND_SWORD, 0, 1));
+				if($this->isBotOnline()) $this->getBot()->getInventory()->sendHeldItem($this->getBot()->getViewers());
 			}
 			break;
 			case 1:
